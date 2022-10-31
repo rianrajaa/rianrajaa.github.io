3ed88e6c1bcf605e4c63693a09a1e45c77451d32
rianrajaa <rianraja754@gmail.com>
2022-10-31 20:59:56 +0700

    my portofolio

--- forms/contact.php
+++ forms/contact.php
@@ -6,7 +6,7 @@
   
   $contact->honeypot = $_POST['first_name'];
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  if( file_exists($php_email_form = 'assets/vendor/php-email-form/PHP-Email-Form-master/contactform.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
